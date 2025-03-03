485e8ecc3c6711ae5bc21c6bd17eecc9553d5d4e
Verneri Niinimäki <71127056+vniinimaki@users.noreply.github.com>
2025-03-03 12:10:27 +0200

    Update locustfile.py
    updated locustfile to send GET requests

--- load-testing/locustfile.py
+++ load-testing/locustfile.py
@@ -1,8 +1,16 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, constant
+import random
 
+firstName = ['John', 'Jane', 'Mike', 'Sarah', 'David', 'Emma', 'James', 'Emily']
+lastName = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis']
+class LoadTestUser(HttpUser):
+    wait_time = constant(1)
+    @task(9)
+    def get_user(self):
+        self.client.get("/resource/" + firstName[random.randint(0,7)] + "%20" + lastName[random.randint(0,7)])
 
-class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
-        # HOST: http://VM_IP:30404/resource/
-        self.client.get("John%20Williams")
+    @task(1)
+    def get_nonexistant(self):
+        with self.client.get("/resource/DoesNot%20Exist", catch_response=True) as response:
+            if response.status_code == 404:
+                response.success()
